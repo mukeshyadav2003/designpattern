bcab70af840edd77c5a08ddd2f31ec6e62217c20
mukeshyadav2003 <mukeshm21102003@gmail.com>
2024-12-19 13:01:03 +0530

    first comit

--- src/AbstractFactory/main.java
+++ src/AbstractFactory/main.java
@@ -3,11 +3,13 @@ package AbstractFactory;
 public class main {
 
 	public static void main(String[] args){
-		
+		                                                            //CourseFactory();
                   SourceCourseFactory course = FactoryCreator.getSourceCourseFactory("course");
                   
                   System.out.println(course.getCourse("programming").getCourseName());
                   
+                  
+                                                                   //SourceFactory();
                   SourceCourseFactory source = FactoryCreator.getSourceCourseFactory("source");
                   
                   System.out.println(source.getSource("online").getSourceName());
